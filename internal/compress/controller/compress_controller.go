e65e6cac80822519364691b9ffce81e2c959f5c1
tudemaha <gede_mahardika88@yahoo.co.id>
2025-02-14 12:33:42 +0800

    feat: add compress handler

---
+++ internal/compress/controller/compress_controller.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/tudemaha/logpress_gateway/internal/compress/service"
+	"github.com/tudemaha/logpress_gateway/internal/global/dto"
+)
+
+func CompressHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		var response dto.Response
+
+		response.DefaultOK()
+		json.NewEncoder(w).Encode(response)
+
+		go service.CreateDump()
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	compressController "github.com/tudemaha/logpress_gateway/internal/compress/controller"
 	pingController "github.com/tudemaha/logpress_gateway/internal/ping"
 )
 
@@ -11,6 +12,7 @@ func LoadRoutes() {
 	log.Println("INFO LoadRoutes: loading routes...")
 
 	http.HandleFunc("/ping", pingController.PingHandler())
+	http.HandleFunc("/compress", compressController.CompressHandler())
 
 	log.Println("INFO LoadRoutes: routes loaded.")
 }

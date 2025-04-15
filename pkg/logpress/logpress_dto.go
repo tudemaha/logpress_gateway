e6526bc59a7b5a8f7c8eb67474760cb9b79f0685
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-04-15 21:25:14 +0800

    fix: update threshold data type on logpress config

--- pkg/logpress/logpress_dto.go
+++ pkg/logpress/logpress_dto.go
@@ -5,7 +5,7 @@ import "time"
 type LogpressConfig struct {
 	Username          string    `json:"username"`
 	Password          string    `json:"password"`
-	Threshold         uint8     `json:"threshold"`
+	Threshold         uint16    `json:"threshold"`
 	ThresholdUnit     string    `json:"threshold_unit"`
 	LastDumpTimestamp time.Time `json:"last_dump_timestamp"`
 	CronInterval      uint8     `json:"cron_interval"`
